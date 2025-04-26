e20e7395db26a781fc2d1dd7ff32512839565249
Vineeth A C <30316226+vineethac@users.noreply.github.com>
2025-04-27 00:31:38 +0530

    initial commit

---
+++ call_azureopenai_model/call_azureopenai.py
@@ -0,0 +1,44 @@
+'''
+Sample code to interact with Azure OpenAI model.
+This is a basic call to chat completion API without streaming response.
+
+'''
+
+from openai import AzureOpenAI
+
+ENDPOINT = "your_endpoint"
+MODEL_NAME = "your_model_name"
+DEPLOYMENT = "your_deployment_name"
+
+SUBSCRIPTION_KEY = "your_api_key"
+API_VERSION = "2024-12-01-preview"
+
+client = AzureOpenAI(
+    api_version = API_VERSION,
+    azure_endpoint = ENDPOINT,
+    api_key = SUBSCRIPTION_KEY,
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful travel planner and assistant.",
+        },
+        {
+            "role": "user",
+            "content": "I am planning for a roadtrip across India for 3 weeks during mid December. \
+                        Where all should I visit? I will be traveling by car from Bangalore. \
+                        Can you plan the itenary for 3 weeks? How about North East?",
+        }
+    ],
+    max_completion_tokens=4096,
+    temperature=1.0,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    model=DEPLOYMENT
+)
+
+print(response.choices[0].message.content)
+client.close()
